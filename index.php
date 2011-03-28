a293098171671e5108ea3995382b38d108365e38
xanm <xanm@mail.ru>
2011-03-29 00:34:36 +0400

    -- fix code style

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $config = dirname(__FILE__) . '/protected/config/main.php';
 
 
 // remove the following line when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 require_once($yii);
 
--- protected/views/file/_manager.php
+++ protected/views/file/_manager.php
@@ -1,82 +1,79 @@
 <p></p>
 <?php
-    $lroot=substr_replace($p,"",0,$len);
-    $lroot=str_replace("\\","/",$lroot);
+  $lroot = substr_replace($p, "", 0, $len);
+  $lroot = str_replace("\\", "/", $lroot);
 
-    $lroot2=substr_replace($p2,"",0,$len);
-    $lroot2=str_replace("\\","/",$lroot2);
-        
-    if($panel=='1')
+  $lroot2 = substr_replace($p2, "", 0, $len);
+  $lroot2 = str_replace("\\", "/", $lroot2);
+
+  if($panel == '1')
+  {
+    echo(" <input id=\"p1\" value=\"$lroot\" type=\"hidden\"/>");
+    echo("<table id=\"stats\" class=\"stats\">");
+  }
+  else
+  {
+    if($panel == '2')
     {
-      echo(" <input id=\"p1\" value=\"$lroot\" type=\"hidden\"/>");
-      echo("<table id=\"stats\" class=\"stats\">");
+      echo(" <input id=\"p2\" value=\"$lroot\" type=\"hidden\"/>");
+      echo("<table id=\"stats2\" class=\"stats\">");
     }
-    else
-    {
-      if($panel=='2')
-      {
-        echo(" <input id=\"p2\" value=\"$lroot\" type=\"hidden\"/>");
-        echo("<table id=\"stats2\" class=\"stats\">");
-      }
-    }    
+  }
 ?>
-  <tbody>
-  <thead>    
+<tbody>
+  <thead>
     <tr>
       <th width="130">Name</th>
       <th width="50">Size</th>
       <th width="20">Type</th>
-            <th width="100">Date last change</th>
+      <th width="100">Date last change</th>
     </tr>
-  </thead>   
-<?php
-    foreach($tree as $i){
-    $tmp=$p.'\\'.$i;
-    
-    if($panel=='1')
+  </thead>
+  <?php
+    foreach($tree as $i)
     {
-      echo ("<tr id=\"df\" ondblclick=\"go('$lroot/$i','$lroot2',$panel,'".Yii::app()->homeUrl."');\">");
-      echo ("<td id=\"work\" class=\"work1\">$i</td>");
-    }
-    else
-    {
-      if($panel=='2')
+      $tmp=$p . '\\' . $i;
+      
+      if($panel == '1')
       {
-        echo ("<tr ondblclick=\"return go('$lroot2','$lroot/$i',$panel,'".Yii::app()->homeUrl."');\">");
+        echo ("<tr id=\"df\" ondblclick=\"go('$lroot/$i','$lroot2',$panel,'" . Yii::app()->homeUrl . "');\">");
+        echo ("<td id=\"work\" class=\"work1\">$i</td>");
+      }
+      elseif($panel=='2')
+      {
+        echo ("<tr ondblclick=\"return go('$lroot2','$lroot/$i',$panel,'" . Yii::app()->homeUrl . "');\">");
         echo ("<td  id=\"work\" class=\"work2\">$i</td>");
       }
-    }
 
-    if($i!='..' && $i!='.')
-    {
-      $fS = stat($tmp);
-      if (!is_dir($tmp))
+      if($i != '..' && $i != '.')
       {
-          
+        $fS = stat($tmp);
+        if (!is_dir($tmp))
           echo("<td>$fS[size] byte</td>");
+        else
+          echo ("<td></td>");
+
+        if (is_dir($tmp))
+          echo ("<td>Dir</td>");
+        if (is_file($tmp))
+          echo ("<td id=\"fil\">File</td>");
+
+        $t = getdate($fS["mtime"]);
+        echo ("<td>" . date("d.m.Y H:i:s.", filemtime($tmp)) . "</td>");
       }
       else
       {
-          echo ("<td></td>");
+        echo ("<td></td>");
+        echo ("<td></td>");
+        echo ("<td></td>");
       }
-
-      if (is_dir($tmp)){echo ("<td>Dir</td>");};
-      if (is_file($tmp)){echo ("<td id=\"fil\">File</td>");};
-      $t=getdate($fS["mtime"]);
-      echo ("<td>".date("d.m.Y H:i:s.",filemtime($tmp))."</td>");
-    }
-    else
-    {
-      echo ("<td></td>");
-      echo ("<td></td>");
-      echo ("<td></td>");
-    }
 
       echo ('</tr>');
     }
-?>
-  </tbody></table>
+  ?>
+  </tbody>
+</table>
 <script type="text/javascript">
-  $('.stats').flexigrid();    
+  $('.stats').flexigrid();
 </script>    
     
